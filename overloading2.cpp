b3346f8272dd8d190b1ad60a419c8fb7bbd2c42a
akshitchordia <chordiaakshit3@gmail.com>
2025-09-17 09:31:52 +0530

    Create overloading2.cpp

---
+++ overloading2.cpp
@@ -0,0 +1,33 @@
+//akshit chordia
+//24070123008
+//entc A1
+
+#include <iostream>
+using namespace std;
+#include <string>
+
+class Name {
+private:
+    double sum;
+public:
+   
+        Name(char A, char B) {
+        cout<<A;
+        cout<<B<<endl;
+    }
+
+
+       Name(string a, string b) {
+        cout  << a+b << endl;
+    }
+};
+    
+int main() {
+    Name A1('A','B');  
+    Name A2("Ishika","Singhal");                 
+    return 0;
+}
+
+//Output
+//AB
+//akshitchordia
