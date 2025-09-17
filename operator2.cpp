7f033b0e4d9326cc37e66f8b037185322a597b15
akshitchordia <chordiaakshit3@gmail.com>
2025-09-17 09:30:30 +0530

    Create operator2.cpp

---
+++ operator2.cpp
@@ -0,0 +1,31 @@
+//akshit chordia
+//24070123008
+//entc A1
+
+#include <iostream>
+using namespace std;
+#include<string>
+
+class Name{
+public:
+string name1; 
+string name2;
+
+Name(string n1, string n2){
+    name1=n1;   
+    name2=n2;   
+}
+
+void add(){
+   cout<<name1+name2;  
+}      
+};
+int main() 
+{
+    Name N1("akshit","chordia");
+    N1.add();
+
+}
+
+//Output
+//akshit chordia
